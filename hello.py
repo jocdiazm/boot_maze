7d2d75216832578cbd8a147ac98d64743a4c9ba0
Jose Carlos <jocdiazmuic@gmail.com>
2024-09-03 17:24:17 -0500

    initial setup

---
+++ hello.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from project-maze-solver!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ main.py
@@ -0,0 +1,12 @@
+def main():
+    x = 10
+    y = 20
+
+    print("Jose carlos")
+    print("Hello world")
+    print("Starting maze solver")
+    print(f"x = {x}, y = {y}")
+
+
+if __name__ == "__main__":
+    main()
